dd0601a0d335f575ed11ad78224e19a3b2832ccb
Данила <danila.prasolov01@gmail.com>
2022-07-13 15:28:33 +0300

    скорректированы сообщения

--- src/main/java/com/company/untitled1/screen/ticket/TicketEdit.java
+++ src/main/java/com/company/untitled1/screen/ticket/TicketEdit.java
@@ -1,6 +1,6 @@
 package com.company.untitled1.screen.ticket;
 
-import com.company.untitled1.app.TicketMeneger;
+import com.company.untitled1.app.TicketManager;
 import io.jmix.ui.Dialogs;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.screen.*;
@@ -16,8 +16,8 @@ public class TicketEdit extends StandardEditor<Ticket> {
 
     @Subscribe("commitAndCloseBtn")
     public void onCommitAndCloseBtnClick(Button.ClickEvent event) {
-            if(!TicketMeneger.checkTicket(this.getEditedEntity().getTrip(), this.getEditedEntity().getDepartureDate(), this.getEditedEntity().getArrivalDate())){
-                dialogs.createMessageDialog().withCaption("Ошибка создания билета").withMessage("проверьте дату отъезда и приезда").show();
+            if(!TicketManager.checkTicket(this.getEditedEntity().getTrip(), this.getEditedEntity().getDepartureDate(), this.getEditedEntity().getArrivalDate())){
+                dialogs.createMessageDialog().withCaption("Ошибка добавления билета").withMessage("В выбранной поездке билет на данный промежуток времени уже существует. Проверьте дату и времени отъезда и прибытия.").show();
             }
             else{
                 this.closeWithCommit();
