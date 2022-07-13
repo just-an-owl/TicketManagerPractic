6375c2e803c8dd3ced981308983ffcb9f338818e
Данила <danila.prasolov01@gmail.com>
2022-07-13 14:36:32 +0300

    добавлен сервис и скорректированы чейнж сеты

---
+++ src/main/java/com/company/untitled1/app/TicketMeneger.java
@@ -0,0 +1,43 @@
+package com.company.untitled1.app;
+
+import com.company.untitled1.entity.Ticket;
+import com.company.untitled1.entity.Trip;
+import io.jmix.core.DataManager;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+public class TicketMeneger {
+    @Autowired
+    private DataManager dataManager;
+    public static boolean checkTicket(Trip trip, LocalDateTime start, LocalDateTime stop){
+        for (Ticket i:trip.getTickets()
+             ) {
+            if (start.getDayOfYear() >= i.getDepartureDate().getDayOfYear()&&
+                    i.getArrivalDate().getDayOfYear()>=start.getDayOfYear()&&
+            i.getDepartureDate().getHour()<=start.getHour()&&
+            i.getDepartureDate().getMinute()<start.getMinute()&&
+            i.getArrivalDate().getHour()>=start.getHour()&&
+            i.getArrivalDate().getMinute()>start.getMinute())
+            {
+                return false;
+            }
+            if (i.getDepartureDate().getDayOfYear()<=stop.getDayOfYear()&&
+            i.getDepartureDate().getHour()<=stop.getHour()&&
+            i.getDepartureDate().getMinute()<stop.getMinute()&&
+            i.getArrivalDate().getDayOfYear()>=stop.getDayOfYear()&&
+            i.getArrivalDate().getHour()>=stop.getHour()&&
+            i.getArrivalDate().getMinute()>stop.getMinute())
+            {
+                return  false;
+            }
+
+        }
+        return true;
+    }
+}
--- src/main/java/com/company/untitled1/screen/ticket/TicketEdit.java
+++ src/main/java/com/company/untitled1/screen/ticket/TicketEdit.java
@@ -1,10 +1,26 @@
 package com.company.untitled1.screen.ticket;
 
+import com.company.untitled1.app.TicketMeneger;
+import io.jmix.ui.Dialogs;
+import io.jmix.ui.component.Button;
 import io.jmix.ui.screen.*;
 import com.company.untitled1.entity.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Ticket.edit")
 @UiDescriptor("ticket-edit.xml")
 @EditedEntityContainer("ticketDc")
 public class TicketEdit extends StandardEditor<Ticket> {
+    @Autowired
+    private Dialogs dialogs;
+
+    @Subscribe("commitAndCloseBtn")
+    public void onCommitAndCloseBtnClick(Button.ClickEvent event) {
+            if(!TicketMeneger.checkTicket(this.getEditedEntity().getTrip(), this.getEditedEntity().getDepartureDate(), this.getEditedEntity().getArrivalDate())){
+                dialogs.createMessageDialog().withCaption("Ошибка создания билета").withMessage("проверьте дату отъезда и приезда").show();
+            }
+            else{
+                this.closeWithCommit();
+            }
+    }
 }
